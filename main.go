ad9be8cf839c9e13399d5ce5316be2300b22e2f7
AnuchitO <anuchitp@thoughtworks.com>
2018-11-22 07:50:38 +0700

    create /upload to upload image   - `curl -F "category=Book" -F "image=@/path/to/image.jpg" http://localhost:1323/upload` TODO: send image to Cloud Vision API to get infomation       and store to DB and return back to client.

--- main.go
+++ main.go
@@ -1,15 +1,51 @@
 package main
 
 import (
+	"io"
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo"
 )
+
+func uploadImage(c echo.Context) error {
+	caty := c.FormValue("category")
+	image, err := c.FormFile("image")
+	if err != nil {
+		return err
+	}
+
+	// Source
+	src, err := image.Open()
+	if err != nil {
+		return err
+	}
+	defer src.Close()
+
+	// Destination
+	dst, err := os.Create(image.Filename)
+	if err != nil {
+		return err
+	}
+	defer dst.Close()
+
+	// Copy
+	if _, err = io.Copy(dst, src); err != nil {
+		return err
+	}
+
+	return c.JSON(http.StatusOK, map[string]string{
+		"category": caty,
+	})
+}
 
 func main() {
 	e := echo.New()
 	e.GET("/start", func(c echo.Context) error {
 		return c.String(http.StatusOK, "staring...")
 	})
+
+	e.POST("/upload", uploadImage)
+
 	e.Logger.Fatal(e.Start(":1323"))
 }
